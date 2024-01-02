3c3ecde88531e21ef704b4c0bfc95bba54189a28
Shubham Shinde <67647243+shubshinde1@users.noreply.github.com>
2024-01-02 11:58:55 +0000

    forin loops

--- iterators /forin.js
+++ iterators /forin.js
@@ -1,8 +1,20 @@
 // for in
 
-
+// Object in for in loop
 const obj = {
     IN: 'india',
     SA: 'south africa',
     USA: 'united states of america'
+}
+
+for (const key in obj) {
+    // console.log(`${key} : ${obj[key]}`);
+}
+
+
+
+// Array in for in loop
+let arr = ["sup", "bat", "xmen"];
+for (const indx in arr) {
+    console.log(`${indx} : ${arr[indx]}`);
 }
